1c70b89e79b60d041f56bdeea919fe4730074d2f
EC424 <eduardo.casiano.348@gmail.com>
2022-04-21 14:48:23 -0500

    tarea media avanzada

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,13 +5,17 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  if (x>y) {return x}
+  else {return y};
 }
 
 function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
-}
+  if (edad>=18) {alert('allowed')}
+  else {alert('no allowed')};
+};
   
 function conection(status) {
   //Recibimos un estado de conexión de un usuario representado por un valor numérico. 
@@ -19,7 +23,9 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
-}
+  if (status==1) {return 'online'}
+}else if (status==2) {return 'Away'}
+else{return 'offline'};
 
 function saludo(idioma) {
   // Devuelve un saludo en tres diferentes lenguajes:
@@ -28,6 +34,10 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if (idioma==='eleman') {return 'Guten Tag'}
+  else if (idioma==='mandarin') {return 'ni hao'}
+  else if(idioma==='ingles'){return 'Hello'}
+  else{return 'hola'};
 }
 
 function colors(color) {
